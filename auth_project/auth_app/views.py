8cf4700fefba1a184b5b1ff6729a6c0a092535cb
Sj-Sarkar <159924003+Sj-Sarkar@users.noreply.github.com>
2024-03-16 08:48:00 +0000

    modified views

--- auth_project/auth_app/views.py
+++ auth_project/auth_app/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,logout
 
 # Create your views here.
-def register(request):
+def register_view(request):
     if(request.method=='post'):
         form=UserCreationForm(request.POST)
         if(form.is_valid()):
             user=form.save()
             login(request,user)
 
-def login(request):
+def login_view(request):
     pass
 
-def dashboard(request):
+def dashboard_view(request):
     pass
 
-def logout(request):
+def logout_view(request):
     pass
 
